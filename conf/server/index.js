cba523379e55af096ed7ff4acf35a5fb7755f610
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-20 20:10:40 +0530

    implement load balancer

--- conf/server/index.js
+++ conf/server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log("This server was hit");
   res.send("Hello World!");
 });
 
---
+++ conf/server/readme.md
@@ -0,0 +1,2 @@
+docker build -t mohit-nagaraj/server-express .
+docker run -p 3001:3000 -d mohit-nagaraj/server-express
